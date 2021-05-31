c7d4bae10a858f83349e9490bd686a2b021afc87
Suban <076bct082.suban@pcampus.edu.np>
2021-05-31 21:04:24 +0545

    added wiki and nisan

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 const discord = require('discord.js');
+const axios = require('axios');
 
-const editJsonFile = require('edit-json-file')
-const file = editJsonFile(path.join(__dirname,'config.json'))
+const editJsonFile = require('edit-json-file');
+const file = editJsonFile(path.join(__dirname, 'config.json'));
 
 const commands = require('./commands.js');
 const welcome = require('./welcome');
@@ -12,76 +13,114 @@ const welcome = require('./welcome');
 const client = new discord.Client();
 
 client.on('ready', () => {
-    console.log("the client has logged in");
+	console.log('the client has logged in');
 
-    commands(client, 'ping', message => {
-        message.channel.send('Pong');
-    })
+	commands(client, 'ping', (message) => {
+		message.channel.send('Pong');
+	});
 
-    commands(client, 'ban', (message) => {
-        const { member, mentions } = message
-    
-        const tag = `<@${member.id}>`
-    
-        if (
-          member.hasPermission('ADMINISTRATOR') ||
-          member.hasPermission('BAN_MEMBERS')
-        ) {
-          const target = mentions.users.first()
-          if (target) {
-            const targetMember = message.guild.members.cache.get(target.id)
-            targetMember.ban()
-            message.channel.send(`${tag} That user has been`)
-          } else {
-            message.channel.send(`${tag} Please specify someone to ban.`)
-          }
-        } else {
-          message.channel.send(
-            `${tag} You do not have permission to use this command.`
-          )
-        }
-      })
-    
-    commands(client, 'kick', (message) => {
-        const { member, mentions } = message
-    
-        const tag = `<@${member.id}>`
-    
-        if (
-          member.hasPermission('ADMINISTRATOR') ||
-          member.hasPermission('KICK_MEMBERS')
-        ) {
-          const target = mentions.users.first()
-          if (target) {
-            const targetMember = message.guild.members.cache.get(target.id)
-            targetMember.kick()
-            message.channel.send(`${tag} That user has kicked`)
-          } else {
-            message.channel.send(`${tag} Please specify someone to kick.`)
-          }
-        } else {
-          message.channel.send(
-            `${tag} You do not have permission to use this command.`
-          )
-        }
-      })
-    
-    commands(client, 'join', message => {
-      client.emit('guildMemberAdd', message.member)
-    })
-    
-    welcome(client);
+	commands(client, 'ban', (message) => {
+		const { member, mentions } = message;
 
-    commands(client, 'msg', message => {
-      const args = message.content.substring(process.env.PREFIX.length).split(" ")
-      args.shift();
-      file.pop("message");
-      file.append("message", args.join(' '))
-      file.save()
-      console.log(file.get());
-    })
-})
+		const tag = `<@${member.id}>`;
+
+		if (
+			member.hasPermission('ADMINISTRATOR') ||
+			member.hasPermission('BAN_MEMBERS')
+		) {
+			const target = mentions.users.first();
+			if (target) {
+				const targetMember = message.guild.members.cache.get(target.id);
+				targetMember.ban();
+				message.channel.send(`${tag} That user has been`);
+			} else {
+				message.channel.send(`${tag} Please specify someone to ban.`);
+			}
+		} else {
+			message.channel.send(
+				`${tag} You do not have permission to use this command.`
+			);
+		}
+	});
+
+	commands(client, 'kick', (message) => {
+		const { member, mentions } = message;
+
+		const tag = `<@${member.id}>`;
+
+		if (
+			member.hasPermission('ADMINISTRATOR') ||
+			member.hasPermission('KICK_MEMBERS')
+		) {
+			const target = mentions.users.first();
+			if (target) {
+				const targetMember = message.guild.members.cache.get(target.id);
+				targetMember.kick();
+				message.channel.send(`${tag} That user has kicked`);
+			} else {
+				message.channel.send(`${tag} Please specify someone to kick.`);
+			}
+		} else {
+			message.channel.send(
+				`${tag} You do not have permission to use this command.`
+			);
+		}
+	});
+
+	commands(client, 'join', (message) => {
+		client.emit('guildMemberAdd', message.member);
+	});
+
+	commands(client, 'wiki', (message) => {
+		const args = message.content
+			.slice(process.env.PREFIX.length)
+			.trim()
+			.split(/ +/);
+		let word = args[1];
+
+		if (!word) {
+			message.channel.send(
+				`Please use the correct format. ${process.env.PREFIX}${args[0]} <search-term>`
+			);
+		} else {
+			let url = 'https://en.wikipedia.org/w/api.php';
+
+			let params = {
+				action: 'opensearch',
+				search: word,
+				limit: '1',
+				namespace: '0',
+				format: 'json',
+			};
+
+			url = url + '?origin=*';
+			Object.keys(params).forEach(function (key) {
+				url += '&' + key + '=' + params[key];
+			});
+
+			axios(url).then(function (response) {
+				message.channel.send(response.data[3]);
+			});
+		}
+	});
+
+	commands(client, 'nisan', (message) => {
+		message.channel.send(
+			'https://cdn.discordapp.com/attachments/744591904427081811/846292516009279529/bitch.gif'
+		);
+	});
+	welcome(client);
+
+	commands(client, 'msg', (message) => {
+		const args = message.content
+			.substring(process.env.PREFIX.length)
+			.split(' ');
+		args.shift();
+		file.pop('message');
+		file.append('message', args.join(' '));
+		file.save();
+		console.log(file.get());
+	});
+});
 
 client.login(process.env.BOT_TOKEN);
-
-
